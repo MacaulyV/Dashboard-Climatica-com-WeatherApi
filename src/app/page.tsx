2e76c666cceb13c526a6587e5ceac9145c9527ba
Macauly <Macaulyv@gmail.com>
2024-04-22 22:09:22 -0300

    Desenvolvendo o Front-end Pt1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,68 @@
-import Image from "next/image";
+"use client";
+import React, { useEffect } from "react";
 import styles from "./page.module.css";
+import { motion } from "framer-motion";
 
 export default function Home() {
+  useEffect(() => {
+    fetch("http://localhost:5000/")
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error("Error:", error));
+  }, []);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div className={styles.Fundo}>
+      <nav className={styles.nav}>
+        <motion.img
+          src="/image/Logo.svg"
+          alt=""
+          className={styles.Logo}
+          initial={{ scale: 0, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 1 }}
         />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+        <motion.h1
+          initial={{ scale: 0, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 1 }}
+          className={styles.Titulo}
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          ClimaticFuture
+        </motion.h1>
+        <form className={styles.BarraPesquisa}>
+          <input type="text" placeholder="Buscar cidade" />
+        </form>
+        <img
+          src="/image/Conta.svg"
+          alt=""
+          className={styles.IconLogin}
+          initial={{ scale: 0, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 1 }}
+        />
+      </nav>
+      <main className={styles.main}>
+        <div className={styles.gridItem}>
+          <h1></h1>
+        </div>
+        <div className={styles.gridItem}>
+          <p>Grid Item 2</p>
+        </div>
+        <div className={styles.gridItem}>
+          <p>Grid Item 3</p>
+        </div>
+      </main>
+      <footer className={styles.footer}>
+      <a href="" className={styles.Link}>Heading</a>
+      <a href="" className={styles.Link}>Heading</a>
+      <a href="" className={styles.Link}>Heading</a>
+      <a href="" className={styles.Link}>Heading</a>
+      <a href="" className={styles.Link}>Heading</a>
+      <a href="" className={styles.Link}>Heading</a>
+      <a href="" className={styles.Link}>Heading</a>
+      <a href="" className={styles.Link}>Heading</a>
+      </footer>
+    </div>
   );
 }
